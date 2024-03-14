541f52f17172881c855779647138755e817cb9e2
Atakan Erkan <atakan.erkan@ogr.dpu.edu.tr>
2024-03-15 00:31:06 +0300

    server starts

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Database Connection
+mongoose.connect();
